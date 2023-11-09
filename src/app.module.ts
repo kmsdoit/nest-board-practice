9152e31e896558287512af1f3a514693ed998d33
kmsdoit <klmmms882912@naver.com>
2023-11-09 12:38:04 +0900

    github ci/cd .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,19 +6,25 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 import { UsersModule } from './users/users.module';
 import { HeartModule } from './heart/heart.module';
 import { CategoryModule } from './category/category.module';
+import {ConfigModule} from "@nestjs/config";
 
 
 @Module({
-  imports: [BoardModule, TypeOrmModule.forRoot({
+  imports: [ConfigModule.forRoot({
+    envFilePath : '.env',
+    isGlobal : true
+  }),
+    BoardModule, TypeOrmModule.forRoot({
     type : 'postgres',
-    host : process.env.DB_HOST,
-    username : 'nest',
-    password : 'nest',
-    database : 'nest',
+    host : process.env.POSTGRES_HOST,
+    username : process.env.POSTGRES_USER,
+    password : process.env.POSTGRES_PASSWORD,
+    database : process.env.POSTGRES_DATABASE,
     synchronize : true,
     entities: [__dirname + '/**/*.entity{.ts,.js}'],
     logging : true
-  }), UsersModule, HeartModule, CategoryModule],
+  }), UsersModule, HeartModule, CategoryModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
